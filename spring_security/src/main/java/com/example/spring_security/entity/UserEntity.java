bd202552ab4f53f3d89210a2a0dc50a842f4ee57
Nguyên dey <143236234+Nguyendinhle1911@users.noreply.github.com>
2025-04-03 11:27:56 +0700

    hihi

---
+++ spring_security/src/main/java/com/example/spring_security/entity/UserEntity.java
@@ -0,0 +1,24 @@
+package com.example.spring_security.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
